ff1c20b87b3a01df61ded531dc35764641e0572f
Shubhamss105 <91242629+Shubhamss105@users.noreply.github.com>
2022-07-27 23:29:24 +0530

    Add files via upload

---
+++ javacsript.js/index.js
@@ -0,0 +1,19 @@
+// function fibonacci(n){
+//     let output=[];
+//     if(n===1){
+//         output=[1];
+//     }
+//     else if(n===2){
+//         output=[0,1];
+//     }
+//     else{
+//         output=[0,1];
+//         for(let  i=2;i<n;i++){
+//             output.push(output[output.length-2] + output[output.lenght-1])
+//         }
+//     }
+// }
+// console.log("Enter the number:", fibonacci(6));
+
+document.querySelector("h1").innerHTML="Good Bye";
+alert("Hello");
